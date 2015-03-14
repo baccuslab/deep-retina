fixed bug in logistic_backward layer, and updated code for masking val and test data

python/utils/classifier_trainer.py
@@ -150,20 +150,9 @@ class ClassifierTrainer(object):
                 batch_mask = np.random.choice(X_train_subset.shape[0], batch_size)
                 X_batch    = X_train_subset[batch_mask]
                 y_batch    = y_train_subset[batch_mask]
-                scores[it*batch_size:(it+1)*batch_size] = loss_function(X_batch, model).squeeze()
-                y_pred_train[it*batch_size:(it+1)*batch_size] = (1./(1 + np.exp(-scores[it*batch_size:(it+1)*batch_size])))
+                y_pred_train[it*batch_size:(it+1)*batch_size] = loss_function(X_batch, model).squeeze()
         else:
-            scores = loss_function(X_train_subset, model)
-            y_pred_train = (1./(1 + np.exp(-scores)))
-        #y_pred_train = []
-        #for i in xrange(X_train_subset.shape[0] / 100):
-        #  X_train_slice = X_train_subset[i*100:(i+1)*100]
-        #  if predict_fn is not None:
-        #    X_train_slice = predict_fn(X_train_slice)
-        #  scores = loss_function(X_train_slice, model)
-        #  y_pred_train.append(np.argmax(scores, axis=1))
-        #y_pred_train = np.hstack(y_pred_train)
-        #train_acc = np.mean(y_pred_train == y_train_subset)
+            y_pred_train = loss_function(X_train_subset, model) # calling loss_function with y=None returns rates
         
         train_acc, _ = pearsonr(y_pred_train.squeeze(), y_train_subset.squeeze()) 
         #train_acc, _ = layers.cross_entropy_loss(scores, y_train_subset)
@@ -180,26 +169,12 @@ class ClassifierTrainer(object):
                 batch_mask  = np.random.choice(X_val.shape[0], batch_size)
                 X_val_batch = X_val[batch_mask]
                 y_val_batch = y_val[batch_mask]
-
-                scores[it*batch_size:(it+1)*batch_size] = loss_function(X_val_batch, model).squeeze()
-                y_pred_val[it*batch_size:(it+1)*batch_size] = (1./(1 + np.exp(-scores[it*batch_size:(it+1)*batch_size])))
+                y_pred_val[it*batch_size:(it+1)*batch_size] = loss_function(X_val_batch, model).squeeze()
         else:
-            scores = loss_function(X_val, model)
-            y_pred_val = (1./(1 + np.exp(-scores)))
+            y_pred_val = loss_function(X_val, model) # calling loss_function with y=None returns rates
 
-        #for i in xrange(int(np.ceil(X_val.shape[0] / 100.0))):
-        #  X_val_slice = X_val[i*100:(i+1)*100]
-        #  if predict_fn is not None:
-        #    X_val_slice = predict_fn(X_val_slice)
-        #  scores = loss_function(X_val_slice, model)
-          #y_pred_val.append(np.argmax(scores, axis=1))
-        #  y_pred_val.append(1./(1 + np.exp(-scores))) # just give the probability of spiking
-        #y_pred_val = np.hstack(y_pred_val)
-        #val_acc = np.mean(y_pred_val ==  y_val)
 
         val_acc, _ = pearsonr(y_pred_val.squeeze(), y_val.squeeze())
-        #val_acc, _ = layers.cross_entropy_loss(scores, y_val)
-        #val_acc = np.mean((y_pred_val - y_val)**2)
         val_acc_history.append(val_acc)
         
         # keep track of the best model based on validation accuracy
@@ -216,7 +191,7 @@ class ClassifierTrainer(object):
                  % (epoch, num_epochs, cost, train_acc, val_acc, learning_rate))
 
     if verbose:
-      print 'finished optimization. best validation accor: %f' % (best_val_acc, )
+      print 'finished optimization. best validation accuracy: %f' % (best_val_acc, )
     # return the best model and the training history statistics
     return best_model, loss_history, train_acc_history, val_acc_history
 

python/utils/classifiers/convnet.py
@@ -61,16 +61,17 @@ def two_layer_convnet(X, model, y=None, reg=0.0, dropout=1.0, top_layer='logisti
 
   # Compute the backward pass
   if top_layer == 'softmax':
-    data_loss, dscores = softmax_loss(rates, y)
+    data_loss, drates = softmax_loss(rates, y)
   elif top_layer == 'mse':
-    data_loss, dscores = mse_loss(rates, y)
+    data_loss, drates = mse_loss(rates, y)
   elif top_layer == 'mqe':
-    data_loss, dscores = mqe_loss(rates, y)
+    data_loss, drates = mqe_loss(rates, y)
   elif top_layer == 'logistic':
-    data_loss, dscores = cross_entropy_loss(rates, y)
+    data_loss, drates = cross_entropy_loss(rates, y)
 
   # Compute the gradients using a backward pass
-  dd1, dW2, db2 = affine_backward(dscores, cache3)
+  de1, dparams  = logistic_backward(drates, cache4)
+  dd1, dW2, db2 = affine_backward(de1, cache3)
   da1 = dropout_backward(dd1, cache2)
   dX,  dW1, db1 = conv_relu_pool_backward(da1, cache1)
 
@@ -80,7 +81,7 @@ def two_layer_convnet(X, model, y=None, reg=0.0, dropout=1.0, top_layer='logisti
   reg_loss = reg * (np.sum(np.abs(W1)) + np.sum(np.abs(W2))) #0.5 * reg * sum(np.sum(W * W) for W in [W1, W2])
 
   loss = data_loss + reg_loss
-  grads = {'W1': dW1, 'b1': db1, 'W2': dW2, 'b2': db2}
+  grads = {'W1': dW1, 'b1': db1, 'W2': dW2, 'b2': db2, 'logistic': dparams}
   
   return loss, grads
 

python/utils/layers.py
@@ -233,12 +233,12 @@ def cross_entropy_loss(x, y):
   - dx:   Gradient of the loss with respect to x
   """
   # if you don't do this step, loss is shape (num_ex, num_ex)
-  #desqueezeX = False
-  #if len(x.shape) > 1:
-  #    x = x.squeeze()
-  #    desqueezeX = True
-  #if len(y.shape) > 1:
-  #    y = y.squeeze()
+  desqueezeX = False
+  if len(x.shape) > 1:
+      x = x.squeeze()
+      desqueezeX = True
+  if len(y.shape) > 1:
+      y = y.squeeze()
 
   x[x==0.0] = 10e-200
   x[x==1.0] = 1. - 10e-15
@@ -252,8 +252,8 @@ def cross_entropy_loss(x, y):
   #    import pdb
   #    pdb.set_trace()
 
-  #if desqueezeX:
-  #    dx = np.expand_dims(dx, 1)
+  if desqueezeX:
+      dx = np.expand_dims(dx, 1)
 
   return loss, dx
 
@@ -303,7 +303,7 @@ def logistic_backward(dout, cache):
   dalpha  = 1. / (1 + np.exp(-b * (x - c)))
   dgain   = - a * (c - x) * np.exp(-b * (x - c)) / ((1 + np.exp(-b * (x - c)))**2)
   dthresh = - a * b * np.exp(-b * (x - c)) / ((1 + np.exp(-b * (x - c)))**2)
-  dparams = np.array([dout * dalpha, dout * dgain, dout * dthresh])
+  dparams = np.array([dalpha.T.dot(dout), dgain.T.dot(dout), dthresh.T.dot(dout)]).squeeze()
 
   return dx, dparams
 
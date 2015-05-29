073cdc30ec242b0e28d503069f69efd27e926ede
Lane McIntosh <lanemcintosh@gmail.com>
2015-05-29 10:37:58 -0700

    no longer getting any errors... but still there's no output and no live bokeh server plotting

--- blocks/mnist_blocks.py
+++ blocks/mnist_blocks.py
@@ -15,7 +15,7 @@ from blocks.algorithms import GradientDescent, Scale
 from blocks.graph import ComputationGraph
 from blocks.bricks.conv import Convolutional, ConvolutionalLayer, ConvolutionalActivation
 from blocks.initialization import IsotropicGaussian, Constant
-from blocks.bricks import Rectifier
+from blocks.bricks import Linear, Rectifier, Softmax
 from blocks.bricks.cost import SquaredError
 
 from fuel.datasets import MNIST
@@ -27,6 +27,8 @@ from blocks.extensions.monitoring import TrainingDataMonitoring
 from blocks.extensions.monitoring import DataStreamMonitoring
 from blocks.extensions.plot import Plot
 
+from blocks.model import Model
+
 ## Load data
 mnist = MNIST("train")
 train_stream = DataStream.default_stream(mnist,
@@ -36,20 +38,31 @@ test_stream  = DataStream.default_stream(mnist,
 
 # MAKE MODEL
 # First convolutional layer
-convlayer = ConvolutionalLayer(Rectifier().apply, filter_size=(11,11), num_filters=2, num_channels=1, batch_size=256, pooling_size=(10,10), image_size=(32,32), weights_init=IsotropicGaussian(), biases_init=Constant(0.01))
-convlayer.initialize()
+print 'Initializing convlayer'
+#convlayer = ConvolutionalLayer(Rectifier().apply, filter_size=(11,11), num_filters=2, num_channels=1, batch_size=256, pooling_size=(2,2), image_size=(28,28), weights_init=IsotropicGaussian(), biases_init=Constant(0.01))
+convlayer = Linear(name='convlayer', input_dim=1*28*28, output_dim=2*14*14)
+#convlayer.initialize()
+
+print 'Initializing affinelayer'
+affinelayer = Linear(name='affinelayer', input_dim=2*14*14, output_dim=1)
 
 # initialize data types
 #x = T.matrix('features')
 x = T.ftensor4('features')
 #y = T.lmatrix('targets')
-y = T.fvector('targets')
-y_hat = convlayer.apply(x)
+y = T.lmatrix('targets')
+#y_hat = convlayer.apply(x)
+convlayer_output = convlayer.apply(x.flatten(2))
+affine_output    = affinelayer.apply(convlayer_output.flatten(2)) # input to affine layer must be 1d
+y_hat            = Softmax().apply(affine_output)
 
 
 # SNAP ON THE LOSS FUNCTION
 print 'Initializing Cost'
-cost = SquaredError().apply(y_hat, y)
+#cost = SquaredError().apply(y_hat, y.flatten())
+y = y.astype('float32') # need this line otherwise next line casts sum to float64
+cost = y_hat.sum() + y.sum()
+cost.name = 'cost'
 
 print 'Initializing Computation Graph'
 cg = ComputationGraph(cost)
@@ -57,7 +70,7 @@ monitor = DataStreamMonitoring(variables=[cost], data_stream=test_stream, prefix
 
 print 'Starting Main Loop'
 main_loop = MainLoop(
-        model=None, data_stream=train_stream,
+        model=Model(cost), data_stream=train_stream,
         algorithm=GradientDescent(cost=cost, params=cg.parameters,
             step_rule=Scale(learning_rate=0.1)),
         extensions=[FinishAfter(after_n_epochs=1),
@@ -67,3 +80,4 @@ main_loop = MainLoop(
 
 main_loop.run()
 
+# model.params

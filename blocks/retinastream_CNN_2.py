b9edefa3adcb3efee13cd8de78c8ab66766aee0c
Niru Maheswaranathan <nirum@stanford.edu>
2015-06-04 11:53:58 -0700

    refactoring

--- blocks/retinastream_CNN_2.py
+++ blocks/retinastream_CNN_2.py
@@ -1,26 +1,23 @@
-# I'll build a convolutional neural network to work on mnist
-
 ##################################################################
 # Building the model
 ##################################################################
-import pdb
 import theano.tensor as T
 import numpy as np
+import os
 
-from blocks.bricks import Rectifier, Softmax, MLP
+from blocks.bricks import Rectifier, MLP
 from extrabricks import SoftRectifier
-from blocks.bricks.cost import SquaredError, MisclassificationRate
-from blocks.bricks.conv import ConvolutionalLayer, ConvolutionalSequence, MaxPooling
-from blocks.bricks.conv import ConvolutionalActivation, Flattener
+from blocks.bricks.cost import SquaredError
+from blocks.bricks.conv import ConvolutionalLayer, ConvolutionalSequence
+from blocks.bricks.conv import Flattener
 from blocks.filter import VariableFilter
-from blocks.initialization import IsotropicGaussian, Constant, Uniform
-from blocks.roles import WEIGHT, FILTER, INPUT
-from blocks.graph import ComputationGraph, apply_dropout
-from blocks.bricks.cost import Cost
-from blocks.bricks.base import application
+from blocks.initialization import IsotropicGaussian, Constant
+from blocks.roles import WEIGHT, FILTER
+from blocks.graph import ComputationGraph
 
 from metrics import PearsonCorrelation, ExplainedVariance, MeanModelRates, PoissonLogLikelihood
 
+# parameters
 batch_size = 256
 image_size = 32
 pooling_size = 2
@@ -32,18 +29,22 @@ epochs = 5
 filter_size = 11
 num_channels = 40   # filter history
 
-x = T.tensor4('data')   # num_examples by num_channels (time history) by image_size by image_size
+# create theano variables for the stimulus (input) and firing rates (output)
+x = T.tensor4('data')
 y = T.fcol('rates')
 
-# Convolutional Layers
+# Build the convolutional Layers
 conv_layers = [
-        ConvolutionalLayer(Rectifier().apply, (filter_size, filter_size), num_filters, (pooling_size, pooling_size), name='l1')]
+    ConvolutionalLayer(Rectifier().apply,
+                       (filter_size, filter_size), num_filters,
+                       (pooling_size, pooling_size), name='l1')]
 
 convnet = ConvolutionalSequence(
-        conv_layers, num_channels=num_channels, image_size=(image_size, image_size),
-        weights_init=IsotropicGaussian(initial_weight_std),
-        biases_init=Constant(0)
-        )
+    conv_layers, num_channels=num_channels,
+    image_size=(image_size, image_size),
+    weights_init=IsotropicGaussian(initial_weight_std),
+    biases_init=Constant(0)
+    )
 
 convnet.initialize()
 
@@ -72,13 +73,11 @@ mse.name    = 'mean_squared_error'
 correlation = PearsonCorrelation().apply(y.flatten(), y_hat.flatten())
 explain_var = ExplainedVariance().apply(y.flatten(), y_hat.flatten())
 mean_y_hat  = MeanModelRates().apply(y_hat.flatten())
-#error_rate = MisclassificationRate().apply(y.flatten(), y_hat)
-#cost = MisclassificationRate().apply(y, y_hat)
-#cost.name = 'error_rate'
 
+# build the computation graph (CG)
 cg = ComputationGraph(cost)
 
-#pdb.set_trace()
+# pick out the weights out of the CG
 weights = VariableFilter(roles=[FILTER, WEIGHT])(cg.variables)
 l2_regularization = 0.005 * sum((W**2).sum() for W in weights)
 
@@ -94,25 +93,15 @@ for layer in convnet.layers:
 # Training
 ##################################################################
 from blocks.main_loop import MainLoop
-from blocks.extensions import SimpleExtension, FinishAfter, Printing
-from blocks.algorithms import GradientDescent, Scale, Momentum
-from blocks.extensions.plot import Plot
-from blocks.extensions.saveload import Checkpoint, LoadFromDump
+from blocks.extensions import FinishAfter, Printing
+from blocks.algorithms import GradientDescent, Momentum
 from blocks.extensions.monitoring import DataStreamMonitoring, TrainingDataMonitoring
 from blocks.model import Model
-#from blocks import MainLoopDumpManager
 
-#from fuel.datasets import MNIST
-from fuel.streams import DataStream
 from retinastream import RetinaStream
 from fuel.schemes import SequentialScheme
-from fuel.transformers import Flatten
 
-import os
-from os.path import expanduser
-import h5py
-
-#rng = np.random.RandomState(1)
+# set a random seed
 seed = np.random.randint(100)
 
 # LOAD DATA
@@ -129,41 +118,39 @@ print 'Loading RetinaStream'
 num_total_examples = 299850
 num_train_examples = 239880     # for 80, 10, 10 split
 num_val_examples   = 29985
-training_stream    = RetinaStream(filename, datadir, cellidx=1, history=num_channels, fraction=0.8, seed=seed,
-        partition_label='train',
-        iteration_scheme=SequentialScheme(num_train_examples, batch_size=batch_size))
-validation_stream  = RetinaStream(filename, datadir, cellidx=1, history=num_channels, fraction=0.8, seed=seed,
-        partition_label='val',
-        iteration_scheme=SequentialScheme(num_val_examples, batch_size=1024))
+training_stream    = RetinaStream(filename, datadir, cellidx=1, history=num_channels,
+                                  fraction=0.8, seed=seed, partition_label='train',
+                                  iteration_scheme=SequentialScheme(num_train_examples, batch_size=batch_size))
+
+validation_stream  = RetinaStream(filename, datadir, cellidx=1, history=num_channels,
+                                  fraction=0.8, seed=seed, partition_label='val',
+                                  iteration_scheme=SequentialScheme(num_val_examples, batch_size=1024))   # note batch size doesn't matter
 
 model = Model(cost_l2)
 algorithm = GradientDescent(
-        cost=cost_l2,
-        params=model.parameters,
-        step_rule=Momentum(
-            learning_rate=0.1,
-            momentum=0.9)
-        )
+    cost=cost_l2,
+    params=model.parameters,
+    step_rule=Momentum(
+        learning_rate=0.1,
+        momentum=0.9)
+    )
 
 main_loop = MainLoop(
-        model = model,
-        data_stream = training_stream,
-        algorithm = algorithm,
-        extensions = [
-            FinishAfter(after_n_epochs=epochs),
-            TrainingDataMonitoring(
-                [cost, correlation, explain_var, mean_y_hat, mse],
-                prefix='train',
-                after_epoch=True),
-            DataStreamMonitoring(
-                [cost, correlation, explain_var, mean_y_hat, mse],
-                validation_stream,
-                prefix='valid'),
-            #Checkpoint('retinastream_model.pkl', after_epoch=True),
-            #EarlyStoppingDump('/Users/jadz/Documents/Micelaneous/Coursework/Blocks/mnist-blocks/', 'valid_error_rate'),
-            #MainLoopDumpManager(expanduser('~/deepretina/blocks/checkpoints'))
-            Printing()
-            ]
-        )
+    model=model,
+    data_stream=training_stream,
+    algorithm=algorithm,
+    extensions=[
+        FinishAfter(after_n_epochs=epochs),
+        TrainingDataMonitoring(
+            [cost, correlation, explain_var, mean_y_hat, mse],
+            prefix='train',
+            after_epoch=True),
+        DataStreamMonitoring(
+            [cost, correlation, explain_var, mean_y_hat, mse],
+            validation_stream,
+            prefix='valid'),
+        Printing()
+        ]
+    )
 
 main_loop.run()

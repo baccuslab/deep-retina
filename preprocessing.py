d5233d0979f79594beff30dc4aa247656b6c1bf1
Lane McIntosh <lanemcintosh@gmail.com>
2015-10-28 16:07:59 -0700

    cleaning up preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,21 +1,6 @@
 from __future__ import absolute_import
 import numpy as np
-import pickle
-from scipy.io import loadmat
-import os.path as path
-import matplotlib
 import h5py
-
-#Force matplotlib to not use any Xwindows else will crash on Rye
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-from scipy.stats import pearsonr
-
-# Keras imports
-from keras.preprocessing import sequence
-import theano
-import theano.tensor as T
 
 def rolling_window(array, window):
 	"""
@@ -51,32 +36,36 @@ def rolling_window(array, window):
 
 def loadData(data_dir):
 	scenes = h5py.File(data_dir, 'r')
-	stim = np.array(scenes['train/stimulus'])
-	stim = stim.T
-	X = rolling_window(stim, 40)
-	X = np.rollaxis(X, 2)
-	X = np.rollaxis(X, 3, 1)
+
+        # TRAIN DATA
+	stim_train = np.array(scenes['train/stimulus'])
+	stim_train = stim_train.T
+	X_train = rolling_window(stim_train, 40)
+	X_train = np.rollaxis(X_train, 2)
+	X_train = np.rollaxis(X_train, 3, 1)
 	#Truncate rates to appropriate time frame
-	y = np.array(scenes['train/response/firing_rate_10ms'])
-	for i in xrange(y.shape[0]): #normalize firing rate of each cell to be between 0 and 1
-		if np.max(y[i]) != 0:
-			y[i] /= np.max(y[i])
-	y = y.T
-	y = y[40:,:]
-	stim_2 = np.array(scenes['test/stimulus'])
-	stim_2 = stim_2.T
-	X_2 = rolling_window(stim_2, 40)
-	X_2 = np.rollaxis(X_2, 2)
-	X_2 = np.rollaxis(X_2, 3, 1)
+	y_train = np.array(scenes['train/response/firing_rate_10ms'])
+	for i in xrange(y_train.shape[0]): #normalize firing rate of each cell to be between 0 and 1
+		if np.max(y_train[i]) != 0:
+			y_train[i] /= np.max(y_train[i])
+	y_train = y_train.T
+	y_train = y_train[40:,:]
+
+        # TEST DATA
+	stim_test = np.array(scenes['test/stimulus'])
+	stim_test = stim_test.T
+	X_test = rolling_window(stim_test, 40)
+	X_test = np.rollaxis(X_test, 2)
+	X_test = np.rollaxis(X_test, 3, 1)
 	#Truncate rates to appropriate time frame
-	y_2 = np.array(scenes['test/response/firing_rate_10ms'])
-	for i in xrange(y_2.shape[0]): #normalize firing rate of each cell to be between 0 and 1
-		if np.max(y_2[i]) != 0:
-			y_2[i] /= np.max(y_2[i])
-	y_2 = y_2.T
-	y_2 = y_2[40:,:]
-	print X.shape
-	print y.shape
-	print X_2.shape
-	print y_2.shape
-	return X, y, X_2, y_2
+	y_test = np.array(scenes['test/response/firing_rate_10ms'])
+	for i in xrange(y_test.shape[0]): #normalize firing rate of each cell to be between 0 and 1
+		if np.max(y_test[i]) != 0:
+			y_test[i] /= np.max(y_test[i])
+	y_test = y_test.T
+	y_test = y_test[40:,:]
+	print X_train.shape
+	print y_train.shape
+	print X_test.shape
+	print y_test.shape
+	return X_train, y_train, X_test, y_test

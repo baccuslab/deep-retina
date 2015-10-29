7ac6ddd3aecbd3e90d5e50e07e58f8af225dff37
Lane McIntosh <lanemcintosh@gmail.com>
2015-10-28 17:22:26 -0700

    added LSTM model to models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.regularizers import l1, l2, activity_l1, activity_l2
 
-def convnet(num_layers=3, filter_sizes=[9], num_filters=[16, 32], pooling_sizes=[2],
+def convnet(num_layers=3, filter_sizes=[9], num_filters=[16, 32, 1], pooling_sizes=[2],
         num_channels=40, data_height=50, l2_regularization=0.0):
     '''
     Returns a convolutional neural network model.
@@ -19,8 +19,7 @@ def convnet(num_layers=3, filter_sizes=[9], num_filters=[16, 32], pooling_sizes=
                         layers will be affine.
     
     num_filters         list of integers; number of filters learned per layer.
-                        If len(num_filters) < len(num_layers), remaining layers
-                        will learn a single filter type.
+                        Should have len(num_filters) = len(num_layers).
     
     pooling_sizes       list of integers; square root of units pooled.
                         len(pooling_size) = number of pooling layers.
@@ -52,14 +51,22 @@ def convnet(num_layers=3, filter_sizes=[9], num_filters=[16, 32], pooling_sizes=
                 W_regularizer=l2(l2_regularization), activation='relu'))
         # if not convolutional, then it's an affine layer
         else:
-            model.add(Dense(num_filters[layer-1] * input_height**2,
-                num_filters[layer], init='normal',
-                W_regularizer=l2(l2_regularization)))
+            # if not last layer, add relu activation to affine layer
+            if layer != num_layers - 1:
+                model.add(Dense(num_filters[layer-1] * input_height**2,
+                    num_filters[layer], init=init_method,
+                    W_regularizer=l2(l2_regularization), activation='relu'))
+            # else it's the last layer and we will add a softplus activation
+            else:
+                model.add(Dense(num_filters[layer-1] * input_height**2,
+                    num_filters[layer], init=init_method,
+                    W_regularizer=l2(l2_regularization)))
 
         # if pooling layer follows convolutional or affine layer
         if layer < num_pooling_layers:
             model.add(MaxPooling2D(poolsize=(pooling_sizes[layer], pooling_sizes[layer]),
                 ignore_border=True))
+            input_height = input_height // 2 
 
         # if it's the last convolutional layer, then need to flatten layer
         if layer == num_convolutional_layers - 1:
@@ -69,3 +76,79 @@ def convnet(num_layers=3, filter_sizes=[9], num_filters=[16, 32], pooling_sizes=
     # to add a softplus activation to the top of the model
     model.add(Activation('softplus'))
 
+    return model
+
+
+
+def lstm(num_layers=3, filter_sizes=[9], num_filters=[16, 32], pooling_sizes=[2],
+        num_channels=40, data_height=50, l2_regularization=0.0):
+    '''
+    Returns a long-short-term-memory (LSTM) network model.
+
+    INPUTS
+    num_layers          integer; number of layers not including input layer
+    
+    filter_sizes        list of integers; size of convolutional filters. If
+                        len(filter_sizes) < len(num_layers), then remaining
+                        layers will be affine.
+    
+    num_filters         list of integers; number of filters learned per layer.
+                        Should have len(num_filters) = len(num_layers).
+    
+    pooling_sizes       list of integers; square root of units pooled.
+                        len(pooling_size) = number of pooling layers.
+
+    num_channels        integer; X.shape[1]; number of time channels in data
+
+    data_height         integer; X.shape[2]; number of pixels on one side of the data
+
+    l2_regularization   float >= 0; strength of L2 regularization
+
+    OUTPUT
+    model               keras model
+    '''
+
+    # Determine architecture
+    num_convolutional_layers = len(filter_sizes)
+    num_pooling_layers = len(pooling_sizes)
+    input_height = data_height
+
+    # Fixed parameters
+    init_method = 'he_uniform'
+
+    # Initialize Feedforward Convnet
+    model = Sequential()
+
+    for layer in range(num_layers)-1:
+        # if convolutional layer
+        if layer < num_convolutional_layers:
+            model.add(TimeDistributedConvolution2D(num_filters[layer], num_channels,
+                filter_sizes[layer], filter_sizes[layer], init=init_method,
+                border_mode='full', subsample=(1,1),
+                W_regularizer=l2(l2_regularization), activation='relu'))
+        # if not convolutional, then it's an affine layer
+        else:
+            # if not last layer, add relu activation to affine layer
+            if layer != num_layers - 1:
+                model.add(TimeDistributedDense(num_filters[layer-1] * input_height**2,
+                    num_filters[layer], init=init_method,
+                    W_regularizer=l2(l2_regularization), activation='relu'))
+
+
+        # if pooling layer follows convolutional or affine layer
+        if layer < num_pooling_layers:
+            model.add(TimeDistributedMaxPooling2D(poolsize=(pooling_sizes[layer], pooling_sizes[layer]),
+                ignore_border=True))
+            input_height = input_height // 2 
+
+        # if it's the last convolutional layer, then need to flatten layer
+        if layer == num_convolutional_layers - 1:
+            model.add(TimeDistributedFlatten())
+
+    model.add(LSTM(input_height, input_height, forget_bias_init='one',
+        return_sequences=True))
+
+    model.add(TimeDistributedDense(input_height, 1, init=init_method,
+        W_regularizer=l2(l2_regularization), activation='softplus'))
+
+    return model

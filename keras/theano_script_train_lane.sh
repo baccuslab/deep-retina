64fab8442587ee75f22f86c31ffd6fb2d3afb085
Lane McIntosh <lanemcintosh@gmail.com>
2015-10-29 14:16:15 -0700

    removed obsolete keras/ folder

--- keras/theano_script_train_lane.sh
+++
@@ -1,4 +0,0 @@
-# Run this on the Rye clusters, after first running the command "module load cuda cudasamples"
-CUDA_ROOT=$CUDA_ROOT:/usr/local/cuda
-export PATH=$PATH:/usr/local/cuda/bin
-THEANO_FLAGS=device=gpu0,floatX=float32 python train-3_layer-convnet.py

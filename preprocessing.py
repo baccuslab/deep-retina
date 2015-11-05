6644d379b8774c97adbbc2b70755796ee3a04bd7
Niru Maheswaranathan <nirum@stanford.edu>
2015-11-04 20:21:22 -0800

    moved rolling_window to utils

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import h5py
 from scipy.stats import zscore
+from utils import rolling_window
 
 __all__ = ['datagen']
 
@@ -74,55 +75,3 @@ def datagen(cellidx, batchsize, expt='15-10-07', filename='naturalscene', method
 
             # yield data
             yield zscore(stim_reshaped[inds, ...]), resp[inds]
-
-
-def rolling_window(array, window, axis=0):
-    """
-    Make an ndarray with a rolling window of the last dimension
-
-    Parameters
-    ----------
-    array : array_like
-        Array to add rolling window to
-
-    window : int
-        Size of rolling window
-
-    axis : 'first' or 'last', optional
-        The axis of the time dimension (default: 'first')
-
-    Returns
-    -------
-    Array that is a view of the original array with a added dimension
-    of size `window`.
-
-    Examples
-    --------
-    >>> x=np.arange(10).reshape((2,5))
-    >>> rolling_window(x, 3)
-    array([[[0, 1, 2], [1, 2, 3], [2, 3, 4]],
-               [[5, 6, 7], [6, 7, 8], [7, 8, 9]]])
-    Calculate rolling mean of last dimension:
-    >>> np.mean(rolling_window(x, 3), -1)
-    array([[ 1.,  2.,  3.],
-               [ 6.,  7.,  8.]])
-
-    """
-
-    # flip array dimensinos if the time axis is the first axis
-    if axis == 0:
-        array = array.T
-
-    elif axis == -1:
-        pass
-
-    else:
-        raise ValueError("Time axis must be 0 or -1")
-
-    assert window >= 1, "`window` must be at least 1."
-    assert window < array.shape[-1], "`window` is too long."
-
-    # with strides
-    shape = array.shape[:-1] + (array.shape[-1] - window, window)
-    strides = array.strides + (array.strides[-1],)
-    return np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)

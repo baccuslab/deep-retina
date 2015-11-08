f7f409d2b9590e1c37948fbd938db85443e28ba5
Lane McIntosh <lanemcintosh@gmail.com>
2015-11-07 16:15:02 -0800

    added affine weight visualization

--- visualizations.py
+++ visualizations.py
@@ -4,12 +4,13 @@ import pyret.filtertools as ft
 import pyret.visualizations as viz
 import json
 import os
-from keras.models import model_from_json
+#from keras.models import model_from_json
 
 pwd = os.getcwd()
 
-def visualize_convnet_filters(weights, title, fig_dir=pwd, fig_size=(8,10), 
-        dpi=500, space=True, time=True, display=False, save=True):
+def visualize_convnet_weights(weights, title='convnet', fig_dir=pwd, 
+        fig_size=(8,10), dpi=500, space=True, time=True, display=False,
+        save=True):
     '''
     Visualize convolutional spatiotemporal filters in a convolutional neural 
     network.
@@ -52,12 +53,12 @@ def visualize_convnet_filters(weights, title, fig_dir=pwd, fig_size=(8,10),
                 spatial,temporal = ft.decompose(weights[plt_idx-1])
                 #plt.subplot(num_rows, num_cols, plt_idx)
                 ax = plt.subplot2grid((num_rows*4, num_cols), (4*y, x), rowspan=3)
-                ax.imshow(spatial) #, clim=[np.min(W0), np.max(W0)])
+                ax.imshow(spatial, interpolation='nearest', cmap='gray') #, clim=[np.min(W0), np.max(W0)])
                 plt.grid('off')
                 plt.axis('off')
                 
                 ax = plt.subplot2grid((num_rows*4, num_cols), (4*y+3, x), rowspan=1)
-                ax.plot(np.linspace(0,400,40), temporal)
+                ax.plot(np.linspace(0,400,40), temporal, 'k', linewidth=2)
                 plt.grid('off')
                 plt.axis('off')
         if display:
@@ -77,7 +78,7 @@ def visualize_convnet_filters(weights, title, fig_dir=pwd, fig_size=(8,10),
                 plt_idx = y * num_cols + x + 1
                 spatial, temporal = ft.decompose(weights[plt_idx-1])
                 plt.subplot(num_rows, num_cols, plt_idx)
-                plt.imshow(spatial, interpolation='nearest', colormap='grey')
+                plt.imshow(spatial, interpolation='nearest', cmap='gray')
                 plt.colorbar()
                 plt.grid('off')
                 plt.axis('off')
@@ -98,7 +99,7 @@ def visualize_convnet_filters(weights, title, fig_dir=pwd, fig_size=(8,10),
                 plt_idx = y * num_cols + x + 1
                 spatial, temporal = ft.decompose(weights[plt_idx-1])
                 plt.subplot(num_rows, num_cols, plt_idx)
-                plt.plot(np.linspace(0,400,40), temporal)
+                plt.plot(np.linspace(0,400,40), temporal, 'k', linewidth=2)
                 plt.grid('off')
                 plt.axis('off')
         if display:
@@ -119,8 +120,59 @@ def visualize_convnet_filters(weights, title, fig_dir=pwd, fig_size=(8,10),
         
 
 
+def visualize_affine_weights(weights, num_conv_filters, title='affine', fig_dir=pwd, 
+        fig_size=(8,10), dpi=500, display=False, save=True):
+    '''
+    Visualize convolutional spatiotemporal filters in a convolutional neural 
+    network.
 
+    Computes the spatial and temporal profiles by SVD.
 
+    INPUTS:
+    weights         weight array of shape (num_filters, history, space, space)
+    title           title of plots; also the saved plot file base name
+    fig_dir         where to save figures
+    fig_size        figure size in inches
+    dpi             resolution in dots per inch
+    display         bool; display figure?
+    save            bool; save figure?
+    
+    OUTPUT:
+    saved figure or displayed figure (or both).
+    '''
+    
+    num_affine_units = weights.shape[1]
+    spatial_size = np.sqrt(weights.shape[0]/num_conv_filters)
+    assert(weights.shape[0] % num_conv_filters == 0, 'Incorrect number of convolutional filters')
 
+    # plot space and time profiles together
+    fig = plt.gcf()
+    fig.set_size_inches(fig_size)
+    plt.title(title, fontsize=20)
+    num_cols = int(num_conv_filters)
+    num_rows = int(num_affine_units)
+    idxs = range(num_cols)
+    for y in range(num_rows):
+        one_unit = weights[:,y].reshape((num_conv_filters, spatial_size, spatial_size))
+        colorlimit = [np.min(one_unit), np.max(one_unit)]
+        for x in range(num_cols):
+            plt_idx = y * num_cols + x + 1
+            plt.subplot(num_rows, num_cols, plt_idx)
+            ax = plt.imshow(one_unit[x], clim=colorlimit, interpolation='nearest', cmap='gray')
+            spatial,temporal = ft.decompose(weights[plt_idx-1])
+            plt.grid('off')
+            plt.xticks([])
+            plt.yticks([])
 
+            if x == 0:
+                if y == int(num_rows/2):
+                    plt.ylabel('%d Units in Affine Layer' %(num_affine_units), fontsize=20)
+            if y == num_rows-1
+                if x == int(num_cols/2):
+                    plt.xlabel('Weights per Convolutional Filter Type', fontsize=20)
+
+    if display:
+        plt.show()
+    if save:
+        plt.savefig(fig_dir + title + '_weights.png', dpi=dpi)
 

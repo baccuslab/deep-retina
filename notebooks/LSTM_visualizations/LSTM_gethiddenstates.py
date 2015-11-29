9ae3f92be8186f53bcccda94286b642dbc93b8f9
anayebi <anayebi@stanford.edu>
2015-11-28 17:20:06 -0800

    Working code now gets outputs of LSTM layer as well as hidden states

--- notebooks/LSTM_visualizations/LSTM_gethiddenstates.py
+++ notebooks/LSTM_visualizations/LSTM_gethiddenstates.py
@@ -1,7 +1,7 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Activation, TimeDistributedDense
-from keras.layers.recurrent import LSTM
+from keras.layers.recurrent import LSTM, LSTMMem
 from keras.regularizers import l2
 from keras.layers.extra import TimeDistributedConvolution2D, TimeDistributedMaxPooling2D, TimeDistributedFlatten
 from keras.optimizers import RMSprop
@@ -53,24 +53,43 @@ def get_outputs(X_batch):
 
     # Add dense (affine) layer with relu activation
     model.add(TimeDistributedDense(num_filters[1], W_regularizer=l2(l2_reg), activation='relu'))
-    if memories:
-        model.add(LSTMMem(100*num_filters[1], return_memories=True))
-    else:
-        # Add LSTM, forget gate bias automatically initialized to 1, default weight initializations recommended
-        model.add(LSTM(100*num_filters[1], return_sequences=True))
+    # Add LSTM, forget gate bias automatically initialized to 1, default weight initializations recommended
+    model.add(LSTM(100*num_filters[1], return_sequences=True))
 
     # # Add a final dense (affine) layer with softplus activation
     model.add(TimeDistributedDense(1, init=weight_init, W_regularizer=l2(l2_reg), activation='softplus'))
     model.compile(loss='poisson_loss', optimizer=RMSmod)
     model.load_weights(weights_dir)
-    get_outputs = theano.function([model.layers[0].input], model.layers[5].get_output(train=False))
-    outputs = get_outputs(X_batch)
+    if not memories:
+        get_outputs = theano.function([model.layers[0].input], model.layers[5].get_output(train=False))
+        outputs = get_outputs(X_batch)
+    else:
+        model2 = Sequential()
+        model2.add(TimeDistributedConvolution2D(num_filters[0], filter_size[0], filter_size[1],
+                                 input_shape=stim_shape, weights=model.layers[0].get_weights(),
+                                 border_mode='same', subsample=(1,1),
+                                 W_regularizer=l2(l2_reg)))
+
+        #Add relu activation separately for threshold visualizations
+        model2.add(Activation('relu'))
+        # max pooling layer
+        model2.add(TimeDistributedMaxPooling2D(pool_size=(2, 2), ignore_border=True))
+
+        # flatten
+        model2.add(TimeDistributedFlatten())
+
+        # Add dense (affine) layer with relu activation
+        model2.add(TimeDistributedDense(num_filters[1], weights=model.layers[4].get_weights(), W_regularizer=l2(l2_reg), activation='relu'))
+        # Add LSTM, forget gate bias automatically initialized to 1, default weight initializations recommended
+        model2.add(LSTMMem(100*num_filters[1], weights=model.layers[5].get_weights(), return_memories=True))
+	model2.compile(loss='poisson_loss', optimizer=RMSmod)
+	outputs = model2.predict(X_batch)
     return outputs
 
 X_test, y_test = load_Data()
 outputs = get_outputs(X_test)
+print outputs.shape
 if not memories:
-    print outputs.shape
     pickle.dump(outputs, open("outputs.p", "wb"))
 else:
     pickle.dump(outputs, open("memories.p", "wb"))

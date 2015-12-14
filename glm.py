1d95e6527e106cbc5c4203b3cbf002b936d27777
Niru Maheswaranathan <nirum@stanford.edu>
2015-12-13 22:57:03 -0800

    adds simple glm script

---
+++ glm.py
@@ -0,0 +1,14 @@
+import limo
+from .preprocessing import loadexpt
+
+# load data
+data = loadexpt([0,1,2,3,4], 'whitenoise', 'train', history=40)
+dt = 1e-2
+
+# GLM features
+cellidx = 0
+f_stim = limo.Feature(data.X)
+opt = limo.PoissonGLM([f_stim], data.y[:, cellidx], dt)
+
+# fit
+opt.fit()

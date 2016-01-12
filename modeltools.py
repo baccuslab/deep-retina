4e704b4c9549662d2e38b23cb1197a3182fe360d
lmcintosh <lmcintosh@stanford.edu>
2016-01-12 09:54:46 -0800

    added more metrics from NEMS

--- modeltools.py
+++ modeltools.py
@@ -76,9 +76,10 @@ def list_layers(model_path, weight_filename):
 
     print(tableprint.hr(3))
 
-def get_performance(model, stim_type='natural', cells=[0]):
+
+def get_test_responses(model, stim_type='natural', cells=[0]):
     '''
-    Get correlation coefficient on held-out data for deep-retina.
+        Get a list of [true_responses, model_responses] on the same test data.
     '''
     if stim_type is 'natural':
         test_data = loadexpt(cells, 'naturalscene', 'test', 40)
@@ -94,15 +95,91 @@ def get_performance(model, stim_type='natural', cells=[0]):
     truth = np.array(truth)
     predictions = np.array(predictions)
 
+    return [truth, predictions]
+
+def cc(r, rhat):
+    """
+    Correlation coefficient
+    """
+    return np.corrcoef(np.vstack((rhat, r)))[0, 1]
+
+
+def lli(r, rhat):
+    """
+    Log-likelihood improvement over a mean rate model (in bits per spike)
+    """
+
+    mean = np.mean(rhat)
+    mu = float(np.mean(r * np.log(mean) - mean))
+    return (np.mean(r * np.log(rhat) - rhat) - mu) / (mean * np.log(2))
+
+
+def rmse(r, rhat):
+    """
+    Root mean squared error
+    """
+    return np.sqrt(np.mean((rhat - r) ** 2))
+
+
+def fev(r, rhat):
+    """
+    Fraction of explained variance
+    """
+
+    mean = np.mean(r)
+    rate_var = np.mean((mean - r) ** 2)
+    return 1.0 - (rmse(r, rhat) ** 2) / rate_var
+
+
+def get_correlation(model, stim_type='natural', cells=[0]):
+    '''
+        Get Pearson's r correlation.
+    '''
+    truth, predictions = get_test_responses(model, stim_type=stim_type, cells=cells)
+
     test_cc = []
     for c in cells:
         test_cc.append(pearsonr(truth[:,c], predictions[:,c])[0])
 
     return test_cc
 
+
+def get_performance(model, stim_type='natural', cells=[0], metric='pearsonr'):
+    '''
+        Get correlation coefficient on held-out data for deep-retina.
+
+        INPUT:
+            model           Keras model
+            stim_type       'natural' or 'white'; which test data to draw from?
+            cells           list of cell indices
+            metric          'pearsonr' (scipy Pearson's r), 
+                            'cc' (numpy's corrcoef),
+                            'lli' (Log-likelihood improvement over a mean rate model in bits per spike),
+                            'rmse' (Root mean squared error),
+                            'fev' (Fraction of explained variance; note this does not take into account
+                                    the variance from trial-to-trial)
+    '''
+    truth, predictions = get_test_responses(model, stim_type=stim_type, cells=cells)
+
+    test_cc = []
+    for c in cells:
+        if metric is 'pearsonr':
+            test_cc.append(pearsonr(truth[:,c], predictions[:,c])[0])
+        elif metric is 'cc':
+            test_cc.append(cc(truth[:,c], predictions[:,c]))
+        elif metric is 'lli':
+            test_cc.append(lli(truth[:,c], predictions[:,c]))
+        elif metric is 'rmse':
+            test_cc.append(rmse(truth[:,c], predictions[:,c]))
+        elif metric is 'fev':
+            test_cc.append(fev(truth[:,c], predictions[:,c]))
+        
+
+    return test_cc
+
 def get_weights(path_to_weights, layer_name='layer_0'):
     '''
-    A simple function to return the weights from a saved .h5 file.
+        A simple function to return the weights from a saved .h5 file.
     '''
     
     weight_file = h5py.File(path_to_weights, 'r')

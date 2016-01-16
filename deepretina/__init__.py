fd94ea35426d6aed0a5ea6a921df581b98186ea8
Niru Maheswaranathan <nirum@stanford.edu>
2016-01-15 18:13:20 -0800

    moving main.py to scripts

--- deepretina/__init__.py
+++ deepretina/__init__.py
@@ -4,12 +4,4 @@ Deep Retina
 
 """
 
-# import main
-# import models
-# import utils
-# import preprocessing
-# # import stimuli
-# # import modeltools
-# import visualizations
-
 __version__ = '0.1.0'

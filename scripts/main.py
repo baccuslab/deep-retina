a5da0523647258b39e277f26da9d13f263f1181a
Niru Maheswaranathan <nirum@stanford.edu>
2016-01-25 19:00:08 -0800

    cleans up main script

--- scripts/main.py
+++ scripts/main.py
@@ -4,13 +4,9 @@ Main script for training deep retinal models
 """
 
 from __future__ import absolute_import
-
-from keras.optimizers import Adam
-from keras.objectives import poisson_loss
-
 from .models import ln, convnet, lstm
 from keras.optimizers import RMSprop
-from keras.objectives import poisson_loss
+
 
 def fit_ln(cell, stimulus_type):
     """
@@ -52,17 +48,28 @@ def fit_convnet(cell, stimulus_type):
 
 
 def fit_lstm(cell, stimulus_type, num_timesteps):
-	RMSmod = RMSprop(lr=0.001, rho=0.99, epsilon=1e-6)
-	mdl = lstm(cell, stimulus_type, num_timesteps=num_timesteps, num_filters=(8,16), filter_size=(13,13), loss='poisson_loss', optimizer=RMSmod, weight_init='he_normal', l2_reg=0.01)
-	batchsize = 100
-	num_epochs = 150
-	save_weights_every = 50
-	mdl.train(batchsize, num_epochs=num_epochs, save_every=save_weights_every)
-	return mdl
+
+    # modified optimizer
+    RMSmod = RMSprop(lr=0.001, rho=0.99, epsilon=1e-6)
+
+    # build the model
+    mdl = lstm(cell, stimulus_type, num_timesteps=num_timesteps,
+               num_filters=(8, 16), filter_size=(13, 13), loss='poisson_loss',
+               optimizer=RMSmod, weight_init='he_normal', l2_reg=0.01)
+
+    # training preferences
+    batchsize = 100
+    num_epochs = 150
+    save_weights_every = 50
+
+    # train
+    mdl.train(batchsize, num_epochs=num_epochs, save_every=save_weights_every)
+
+    return mdl
 
 
 if __name__ == '__main__':
-    #pass
+    pass
     # mdl = fit_lstm(4, 'naturalscene', 152)
     # mdl = fit_ln(0, 'whitenoise')
     # mdl = fit_convnet([0,1,2,3,4], 'naturalscene')

0813ba6850353fefbd76c4cde20f9bb64e0a2b78
lmcintosh <lmcintosh@stanford.edu>
2016-02-03 10:49:31 -0800

    removed dependency on pyret.visualizations since it was causing issues

--- scripts/save_stas.py
+++ scripts/save_stas.py
@@ -6,6 +6,7 @@ import pyret.filtertools as ft
 import pyret.visualizations as pyviz
 import matplotlib.pyplot as plt
 from scipy.stats import zscore
+from matplotlib import gridspec
 
 expt = raw_input("Please type in the experiment date (e.g. 15-10-07) 'Enter': ")
 data_dir = expanduser('~/experiments/data/')
@@ -36,10 +37,31 @@ for c in range(ncells):
             sta += response * stimulus[idx-filter_length:idx]
 
     # decompose STA
-    #space, time = ft.decompose(sta)
+    spatial_profile, temporal_filter = ft.decompose(sta)
 
     # plot STA
     fig = plt.figure(figsize=(6,10))
-    fig, ax = pyviz.plotsta(np.linspace(0.0,filter_length*10.0, filter_length), sta, fig=fig)
+    time = np.linspace(0.0, filter_length*10.0, filter_length)
+    #fig, ax = pyviz.plotsta(np.linspace(0.0,filter_length*10.0, filter_length), sta, fig=fig)
+
+    # build the figure
+    gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1])
+
+    # plot spatial profile
+    ax = fig.add_subplot(gs[0])
+    spatial_profile -= np.mean(spatial_profile)
+    maxval = np.max(np.abs(spatial_profile))
+    img = ax.imshow(spatial_profile, cmap='seismic_r', interpolation='nearest',
+            aspect='equal', vmin=-maxval, vmax=maxval)
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    # plot temporal profile
+    ax = fig.add_subplot(gs[1])
+    ax.plot(time, temporal_filter, linestyle='-', linewidth=2, color='LightCoral')
+    ax.set_xlim(time[0], time[-1])
+    plt.xlabel('Time (ms)', fontsize=20)
+
+
     plt.savefig(data_dir + expt + '/' + 'cell%02i.jpg' %(c+1))
     plt.close()

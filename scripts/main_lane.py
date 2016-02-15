a89a9fa4084fafc4bb06da5d99359d7f5fe67075
lmcintosh <lmcintosh@stanford.edu>
2016-02-14 21:39:11 -0800

    adding support for generalized convnet

--- scripts/main_lane.py
+++ scripts/main_lane.py
@@ -4,7 +4,7 @@ Main script for training deep retinal models
 """
 
 from __future__ import absolute_import
-from deepretina.models import sequential, convnet, train
+from deepretina.models import sequential, convnet, train, generalizedconvnet
 from deepretina.experiments import Experiment
 from deepretina.io import Monitor, main_wrapper
 
@@ -38,6 +38,38 @@ def fit_convnet(cells, stimulus, exptdate, readme=None):
 
     return model
 
+@main_wrapper
+def fit_generalizedconvnet(cells, stimulus, exptdate, readme=None):
+    """Main script for fitting a convnet
+    
+    author: Lane McIntosh
+    """
+
+    stim_shape = (40, 50, 50)
+    ncells = len(cells)
+    batchsize = 50
+
+    # get the convnet layers
+    layers = generalizedconvnet(stim_shape, ncells, 
+            architecture=('conv', 'relu', 'conv', 'relu', 'flatten', 'affine'),
+            num_filters=(8, -1, 25), filter_sizes=(13, -1, 13), weight_init='normal',
+            l2_reg=0.01)
+
+    # compile the keras model
+    model = sequential(layers, 'adam')
+
+    # load experiment data
+    data = Experiment(exptdate, cells, stimulus, stim_shape[0], batchsize)
+
+    # create a monitor to track progress
+    monitor = Monitor('convnet', model, data, readme, save_every=10)
+
+    # train
+    train(model, data, monitor, num_epochs=100)
+
+    return model
+
 
 if __name__ == '__main__':
-    mdl = fit_convnet(list(range(37)), 'whitenoise', 'all-cells', readme='Training model on all good cells in first three experiments')
+    #mdl = fit_convnet(list(range(37)), 'whitenoise', 'all-cells')
+    mdl = fit_generalizedconvnet(list(range(37)), 'whitenoise', 'all-cells')

08e3e67a826d200195302f17533b1f89b9eff816
lmcintosh <lmcintosh@stanford.edu>
2016-02-20 21:37:17 -0800

    modifying fixedlstm script

--- scripts/main_lane.py
+++ scripts/main_lane.py
@@ -77,22 +77,21 @@ def fit_fixedlstm(cells, train_stimuli, test_stimuli, exptdate, readme=None):
     author: Lane McIntosh
     """
 
-    stim_shape = (64,)
+    input_shape = (1000,64)
     ncells = len(cells)
     batchsize = 1000
 
     # get the convnet layers
-    layers = fixedlstm(stim_shape, ncells, num_filters=(8, 64),
-                     filter_size=(13, 13), weight_init='normal', l2_reg=0.01)
+    layers = fixedlstm(input_shape, len(cells), num_hidden=1600, weight_init='normal', l2_reg=0.01)
 
     # compile the keras model
     model = sequential(layers, 'adam', loss='sub_poisson_loss')
 
     # load experiment data
-    data = Experiment(exptdate, cells, train_stimuli, test_stimuli, stim_shape[0], batchsize)
+    data = Experiment(exptdate, cells, train_stimuli, test_stimuli, input_shape[0], batchsize)
 
     # create a monitor to track progress
-    monitor = Monitor('convnet', model, data, readme, save_every=10)
+    monitor = Monitor('fixedlstm', model, data, readme, save_every=10)
 
     # train
     train(model, data, monitor, num_epochs=100)
@@ -106,4 +105,5 @@ if __name__ == '__main__':
     # [0,2,7,10,11,12,31] for '16-01-07'
     # [0,3,7,9,11] for '16-01-08'
     #mdl = fit_convnet(list(range(37)), 'naturalscene', 'all-cells')
-    mdl = fit_convnet([0,2,7,10,11,12,31], ['whitenoise', 'naturalscene', 'naturalmovie'], ['whitenoise', 'naturalscene', 'naturalmovie', 'structured'], '16-01-07')
+    #mdl = fit_convnet([0,2,7,10,11,12,31], ['whitenoise', 'naturalscene', 'naturalmovie'], ['whitenoise', 'naturalscene', 'naturalmovie', 'structured'], '16-01-07')
+    mdl = fit_fixedlstm(list(range(37)), ['whitenoise_affine'], ['whitenoise_affine'], 'all-cells')

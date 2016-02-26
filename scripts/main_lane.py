97ef2b9237f39b923c08b68d3967d48c14f69f55
Lane McIntosh <lmcintosh@stanford.edu>
2016-02-26 10:52:33 -0800

    running models during #cosyne16

--- scripts/main_lane.py
+++ scripts/main_lane.py
@@ -21,7 +21,7 @@ def fit_convnet(cells, train_stimuli, test_stimuli, exptdate, readme=None):
     batchsize = 5000
 
     # get the convnet layers
-    layers = convnet(stim_shape, ncells, num_filters=(8, 64),
+    layers = convnet(stim_shape, ncells, num_filters=(16, 48),
                      filter_size=(13, 13), weight_init='normal', l2_reg=0.01)
 
     # compile the keras model
@@ -102,8 +102,11 @@ def fit_fixedlstm(cells, train_stimuli, test_stimuli, exptdate, readme=None):
 
 if __name__ == '__main__':
     # list(range(37)) for 'all-cells'
+    # [6,10,12,13] for '15-11-21a'
     # [0,2,7,10,11,12,31] for '16-01-07'
     # [0,3,7,9,11] for '16-01-08'
+    gc_15_11_21b = [0,1,3,4,5,8,9,13,14,16,17,18,19,20,21,22,23,24,25]
     #mdl = fit_convnet(list(range(37)), 'naturalscene', 'all-cells')
     #mdl = fit_convnet([0,2,7,10,11,12,31], ['whitenoise', 'naturalscene', 'naturalmovie'], ['whitenoise', 'naturalscene', 'naturalmovie', 'structured'], '16-01-07')
-    mdl = fit_fixedlstm(list(range(37)), ['whitenoise_affine'], ['whitenoise_affine'], 'all-cells')
+    #mdl = fit_fixedlstm(list(range(37)), ['whitenoise_affine'], ['whitenoise_affine'], 'all-cells')
+    mdl = fit_convnet(gc_15_11_21b, ['whitenoise'], ['whitenoise', 'naturalscene'], '15-11-21b')

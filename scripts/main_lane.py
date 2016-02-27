026f9595f35f02b759000f7ea4219403fafa739f
Lane McIntosh <lmcintosh@stanford.edu>
2016-02-26 21:40:40 -0800

    training a model with batchnorm

--- scripts/main_lane.py
+++ scripts/main_lane.py
@@ -41,7 +41,7 @@ def fit_convnet(cells, train_stimuli, test_stimuli, exptdate, readme=None):
 
 
 @main_wrapper
-def fit_generalizedconvnet(cells, stimulus, exptdate, readme=None):
+def fit_generalizedconvnet(cells, train_stimuli, test_stimuli, exptdate, readme=None):
     """Main script for fitting a convnet
 
     author: Lane McIntosh
@@ -49,22 +49,22 @@ def fit_generalizedconvnet(cells, stimulus, exptdate, readme=None):
 
     stim_shape = (40, 50, 50)
     ncells = len(cells)
-    batchsize = 1000
+    batchsize = 5000
 
     # get the convnet layers
     layers = generalizedconvnet(stim_shape, ncells, 
-            architecture=('conv', 'relu', 'conv', 'relu', 'conv', 'relu', 'conv', 'relu', 'conv', 'relu', 'flatten', 'affine'),
-            num_filters=[8, -1, 16, -1, 16, -1, 16, -1, 32], filter_sizes=[5, -1, 5, -1, 5, -1, 5, -1, 3], weight_init='normal',
-            l2_reg=0.02)
+            architecture=('conv', 'batchnorm', 'relu', 'flatten', 'affine', 'batchnorm', 'relu', 'flatten', 'affine'),
+            num_filters=[8, -1, -1, -1, 16], filter_sizes=[13], weight_init='normal',
+            l2_reg=0.01)
 
     # compile the keras model
     model = sequential(layers, 'adam', loss='sub_poisson_loss')
 
     # load experiment data
-    data = Experiment(exptdate, cells, stimulus, stim_shape[0], batchsize)
+    data = Experiment(exptdate, cells, train_stimuli, test_stimuli, stim_shape[0], batchsize)
 
     # create a monitor to track progress
-    monitor = Monitor('convnet', model, data, readme, save_every=50)
+    monitor = Monitor('convnet', model, data, readme, save_every=10)
 
     # train
     train(model, data, monitor, num_epochs=100)
@@ -112,4 +112,5 @@ if __name__ == '__main__':
     #mdl = fit_convnet(list(range(37)), 'naturalscene', 'all-cells')
     #mdl = fit_convnet([0,2,7,10,11,12,31], ['whitenoise', 'naturalscene', 'naturalmovie'], ['whitenoise', 'naturalscene', 'naturalmovie', 'structured'], '16-01-07')
     #mdl = fit_fixedlstm(list(range(37)), ['whitenoise_affine'], ['whitenoise_affine'], 'all-cells')
-    mdl = fit_convnet(gc_15_10_07, ['whitenoise'], ['whitenoise', 'naturalscene'], '15-10-07')
+    #mdl = fit_convnet(gc_15_10_07, ['whitenoise'], ['whitenoise', 'naturalscene'], '15-10-07')
+    mdl = fit_generalizedconvnet(gc_15_10_07, ['whitenoise'], ['whitenoise', 'naturalscene'], '15-10-07')

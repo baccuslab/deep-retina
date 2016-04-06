e457f8e5885155174bfd9dbb1fe5b9bb71cded4a
lmcintosh <lmcintosh@stanford.edu>
2016-04-06 13:10:56 -0700

    fixed :bug: in assertion error

--- deepretina/toolbox.py
+++ deepretina/toolbox.py
@@ -272,8 +272,7 @@ def load_model(model_path, weight_filename, changed_params=None):
                     for i in idxs:
                         arch['layers'][i]['p'] = changed_params['dropout']
                 else:
-                    err_msg = 'Key %s not recognized by load_model at this time.' %key
-                    assert False err_msg
+                    assert False 'Key %s not recognized by load_model at this time.' %key
  
             # saved revised architecture.json file
             with open(os.path.join(model_path, architecture_filename), 'w') as outfile:

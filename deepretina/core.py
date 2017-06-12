dd21e1385c2bc865db069c310b5d944754d877ca
Niru Maheswaranathan <nirum@stanford.edu>
2017-06-12 15:56:13 -0700

    updated core script

--- deepretina/core.py
+++ deepretina/core.py
@@ -1,65 +1,51 @@
 """
 Core tools for training models
 """
-from time import time
-import tableprint as tp
+import os
+from datetime import datetime
+
+import deepdish as dd
+import keras.callbacks as cb
+from keras.layers import Input
+from keras.metrics import mse
+from keras.models import load_model
+from keras.optimizers import Adam
+
+from deepretina.metrics import kcc as cc
 
 __all__ = ['train']
 
 
-def train(model, experiment, monitor, num_epochs, shuffle=True):
-    """Train the given network against the given data
+def load(filepath):
+    """Reload a keras model"""
+    return load_model(filepath, custom_objects={'cc': cc})
 
-    Parameters
-    ----------
-    model : keras.models.Model or glms.GLM
-        A GLM or Keras Model object
 
-    experiment : experiments.Experiment
-        An Experiment object
+def train(model, data, lr=1e-2, bz=5000, nb_epochs=500, val_split=0.05):
+    """Trains a model"""
 
-    monitor : io.Monitor
-        Saves the model parameters and plots of performance progress
+    # build the model
+    n_cells = data.y.shape[1]
+    x = Input(shape=data.X.shape[1:], name='stimulus')
+    mdl = model(x, n_cells)
 
-    num_epochs : int or iterable
-        Number of epochs to train for
-    """
-    # initialize training iteration
-    iteration = 0
-    train_start = time()
+    # compile the model
+    mdl.compile(loss='poisson', optimizer=Adam(lr), metrics=[cc, mse])
 
-    # loop over epochs
-    try:
-        for epoch in range(num_epochs):
-            tp.banner('Epoch #{} of {}'.format(epoch + 1, num_epochs))
-            print(tp.header(["Iteration", "Loss", "Runtime"]), flush=True)
+    # store results in this directory
+    name = mdl.name + datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
+    base = f'../results/{name}'
+    os.mkdir(base)
 
-            # loop over data batches for this epoch
-            for X, y in experiment.train(shuffle=shuffle):
+    # define model callbacks
+    cbs = [cb.ModelCheckpoint(os.path.join(base, 'weights.{epoch:02d}-{val_loss:.2f}.h5')),
+           cb.TensorBoard(log_dir=base, histogram_freq=1, batch_size=5000, write_grads=True),
+           cb.ReduceLROnPlateau(min_lr=0, factor=0.2, patience=10),
+           cb.CSVLogger(os.path.join(base, 'training.csv')),
+           cb.EarlyStopping(monitor='val_loss', patience=10)]
 
-                # update on save_every, assuming it is positive
-                if (monitor is not None) and (iteration % monitor.save_every == 0):
-
-                    # performs validation, updates performance plots, saves results to dropbox
-                    monitor.save(epoch, iteration, X, y, model.predict)
-
-                # train on the batch
-                tstart = time()
-                loss = model.train_on_batch(X, y)
-                elapsed_time = time() - tstart
-
-                # update
-                iteration += 1
-                print(tp.row([iteration, float(loss), tp.humantime(elapsed_time)]), flush=True)
-
-            print(tp.bottom(3))
-
-    except KeyboardInterrupt:
-        print('\nCleaning up')
-
-    # allows the monitor to perform any post-training visualization
-    if monitor is not None:
-        elapsed_time = time() - train_start
-        monitor.cleanup(iteration, elapsed_time)
-
-    tp.banner('Training complete!')
+    # train
+    history = mdl.fit(x=data.X, y=data.y, batch_size=bz, epochs=nb_epochs,
+                      callbacks=cbs, validation_split=val_split, shuffle=True)
+    dd.io.save(os.path.join(base, 'history.h5'), history.history)
+    return history

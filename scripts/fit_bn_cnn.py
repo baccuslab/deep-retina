72b7cc9f85e9737ea2949ffac49fb946cad23ac7
Niru Maheswaranathan <nirum@stanford.edu>
2017-06-13 19:28:57 -0700

    restricting to one gpu

--- scripts/fit_bn_cnn.py
+++ scripts/fit_bn_cnn.py
@@ -1,6 +1,7 @@
 """
 fit BN_CNN models
 """
+import tensorflow as tf
 from deepretina.core import train
 from deepretina.models import bn_cnn
 from deepretina.experiments import loadexpt, CELLS
@@ -10,7 +11,8 @@ if __name__ == '__main__':
     expts = ('15-10-07', '15-11-21a', '15-11-21b')
     stims = ('whitenoise', 'naturalscene')
 
-    for expt in expts:
-        for stim in stims:
-            data = loadexpt(expt, CELLS[expt], stim, 'train', 40, 6000)
-            train(bn_cnn, data, lr=1e-2, nb_epochs=250, val_split=0.05)
+    with tf.device('/gpu:0'):
+        for expt in expts:
+            for stim in stims:
+                data = loadexpt(expt, CELLS[expt], stim, 'train', 40, 6000)
+                train(bn_cnn, data, lr=1e-2, nb_epochs=250, val_split=0.05)

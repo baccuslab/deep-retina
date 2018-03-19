8161b8c7b850f7eedbe5adf8f7bc596945d1506b
Tyler Benster <tbenst@gmail.com>
2018-03-19 09:59:55 -0700

    decoder_nm

--- scripts/decoder.py
+++ scripts/decoder.py
@@ -31,12 +31,16 @@ import deepretina.utils
 
 D = deepretina
 ExptSequence = D.experiments.ExptSequence
-# config.results_dir = "/home/tyler/scratch/results/"
-# config.results_dir = "/home/tyler/results/"
+config.results_dir = "/home/tyler/scratch/results/"
 config.results_dir = "/home/tyler/results/"
-config.data_dir = "/home/salamander/experiments/data/"
+config.results_dir = "/storage/baccus/results/"
+# config.results_dir = "/home/tyler/results/"
+# config.data_dir = "/home/salamander/experiments/data/"
+config.data_dir = "/storage/baccus/"
 
 weights = Path(config.results_dir) / "G-CNN_josh_naturalmovie_2018.02.22-11.43.29" / "weights-066--5.115.h5"
+weights = Path(config.results_dir) / "G-CNN_10_files_nogaussian_2018.02.19-22.24.53" / "weights-100--5.664.h5"
+weights = Path(config.results_dir) / "G-CNN_17-11-10_17-12-09_naturalmovie_2018.03.12-11.16.03" / "weights-068--4.915.h5"
 model = load_model(weights, custom_objects={"tf": tf, "ResizeMethod": D.models.ResizeMethod,
     "argmin_poisson": D.metrics.argmin_poisson,
     "matched_cc": D.metrics.matched_cc,
@@ -46,6 +50,7 @@ model = load_model(weights, custom_objects={"tf": tf, "ResizeMethod": D.models.R
 
 
 datafile = Path(config.data_dir) / "17-12-09b-ssb" / "naturalmovie.h5"
+datafile = Path(config.data_dir) / "17-12-16-ssb" / "naturalmovie.h5"
 data = h5py.File(datafile, mode='r')
 
 # %%
@@ -65,7 +70,7 @@ train_data = ExptSequence([
 valid_data = ExptSequence([
     D.experiments.Exptdata(x_train[ntrain:ntrain+nvalid],y_train[ntrain:ntrain+nvalid])
     ])
-run_name = "Decoder_naturalmovie"
+run_name = "Decoder_naturalmovie_17-11-10_17-12-09"
 input_shape = train_data[0][0].shape[1:]
 train_data[0][1].shape
 steps_per_epoch = len(train_data)
